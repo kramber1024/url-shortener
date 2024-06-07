5e7dd82887e5a362017e819fbd090326ae58bc35
kramber <kramber268@gmail.com>
2024-06-07 10:08:51 +0300

    feat: move id generator to database package feat: rename method and make it property

--- app/core/database/generator.py
+++ app/core/database/generator.py
@@ -1,14 +1,17 @@
 from snowflake import SnowflakeGenerator
 
+from app.core.config import settings
 
-class Generator:
+
+class IDGenerator:
     __snowflake: SnowflakeGenerator
 
     def __init__(self, worker_id: int) -> None:
         self.__snowflake = SnowflakeGenerator(worker_id)
 
-    def new_id(self) -> int:
+    @property
+    def id(self) -> int:
         return int(next(self.__snowflake))
 
 
-gen: Generator = Generator(1023)
+gen: IDGenerator = IDGenerator(settings.db.gen.WORKER_ID)
