f5a2fe9c94d5b0d823853279953b665e780b05aa
kramber <kramber268@gmail.com>
2024-06-09 23:31:56 +0300

    feat: Add cr for Users table

---
+++ app/core/crud.py
@@ -0,0 +1,43 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.database.models import User
+
+if TYPE_CHECKING:
+    from sqlalchemy.engine import Result
+
+
+async def create_user(
+    *,
+    session: AsyncSession,
+    name: str,
+    email: str,
+    password: str,
+) -> User:
+
+    user: User = User(
+        name=name,
+        email=email,
+        password=password,
+    )
+
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
+
+
+async def get_user_by_email(
+    *,
+    session: AsyncSession,
+    email: str,
+) -> User | None:
+
+    result: Result[tuple[User]] = await session.execute(
+        select(User).filter(User.email == email),
+    )
+    user: User | None = result.scalars().first()
+
+    return user
