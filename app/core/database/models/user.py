0807aa45e8de7bdd16a0116e8f53bc649778b0d3
kramber <kramber268@gmail.com>
2024-06-09 22:56:13 +0300

    feat: Add hashing for passwords feat: Add repr and method for checking password

--- app/core/database/models/user.py
+++ app/core/database/models/user.py
@@ -1,3 +1,4 @@
+from bcrypt import checkpw, gensalt, hashpw
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -9,10 +10,16 @@ class User(Base):
 
     name: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(String(64), nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
 
     def __init__(self, *, name: str, email: str, password: str) -> None:
         super().__init__()
         self.name = name
         self.email = email
-        self.password = password
+        self.password = hashpw(password.encode("utf-8"), gensalt(rounds=15)).decode()
+
+    def __repr__(self) -> str:
+        return f"<User {self.id} {self.name}>"
+
+    def is_password_valid(self, password: str) -> bool:
+        return checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
