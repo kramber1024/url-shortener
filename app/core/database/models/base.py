bfd0449a9f344860e603fd5414e5230552d3552c
kramber <kramber268@gmail.com>
2024-06-09 15:07:59 +0300

    feat: Add constructor feat: Add auto snowflakeid generation

--- app/core/database/models/base.py
+++ app/core/database/models/base.py
@@ -1,8 +1,19 @@
 from sqlalchemy import Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+from app.core.database.generator import gen
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        unique=True,
+        sort_order=-1,
+    )
+
+    def __init__(self) -> None:
+        self.id = gen.id
