8a6ee3e054cf972effc4850a359c3be02bd790e6
kramber <kramber268@gmail.com>
2024-06-09 15:07:30 +0300

    feat: Add constructor and increase field name to 32 chars

--- app/core/database/models/user.py
+++ app/core/database/models/user.py
@@ -7,6 +7,12 @@ from .base import Base
 class User(Base):
     __tablename__ = "Users"
 
-    name: Mapped[str] = mapped_column(String(16), nullable=False)
+    name: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(64), nullable=False)
+
+    def __init__(self, *, name: str, email: str, password: str) -> None:
+        super().__init__()
+        self.name = name
+        self.email = email
+        self.password = password
