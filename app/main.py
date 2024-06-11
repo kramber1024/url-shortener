fcc4d315238b400f37e6f69d2fee32b46c105787
kramber <kramber268@gmail.com>
2024-06-11 16:06:06 +0300

    feat: Add error_exception_handler to app handlers

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ import asyncio
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
 from app.api import apis_router
+from app.api.v1.exceptions import ErrorException
+from app.api.v1.handlers import error_exception_handler, validation_exception_handler
 from app.core.database import db
 
 app: FastAPI = FastAPI()
 app.include_router(apis_router)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(ErrorException, error_exception_handler)
 
 
 async def main() -> None:
