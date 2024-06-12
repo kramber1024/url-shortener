cc69ba49cf9a5471396aaf11991a7dd71945f2d8
kramber <kramber268@gmail.com>
2024-06-12 23:26:06 +0300

    feat: Add settings for JWT

--- app/core/config.py
+++ app/core/config.py
@@ -17,6 +17,15 @@ class DatabaseSettings(BaseModel):
     URL: str = f"sqlite+aiosqlite:///{BASE_DIRERCTORY/"core"/"database"/"database.sqlite3"}"
 
 
+# TODO(kramber): Use env for sensitive data.
+# 001
+class JWTSettings(BaseModel):
+    ALGORITHM: Literal["HS256"] = "HS256"
+    SECRET: str = "4ede672801460916ec9f05540cf4325a9800c92e687df0846eeb87b8ad480a4e"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+
+
 class AppSettings(BaseModel):
     NAME: str = "ushort"
 
@@ -30,6 +39,7 @@ class Settings(BaseSettings):
     db: DatabaseSettings = DatabaseSettings()
     app: AppSettings = AppSettings()
     env: EnviromentSettings = EnviromentSettings()
+    jwt: JWTSettings = JWTSettings()
 
 
 settings: Settings = Settings()
