f7ebdd89702d8cbc2cee72b9c774724936c82d05
kramber <kramber268@gmail.com>
2024-06-13 18:36:52 +0300

    feat: Add documentation for 500 error for every endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,25 @@
 from fastapi import APIRouter
 
 from .routes import auth, users
+from .schemes import ErrorResponse as ErrorResponseScheme
 
-api_router: APIRouter = APIRouter(prefix="/v1")
+api_router: APIRouter = APIRouter(
+    prefix="/v1",
+    responses={
+        500: {
+            "description": "Something went very wrong. Please report this issue.",
+            "model": ErrorResponseScheme,
+            "content": {
+                "application/json": {
+                    "example": {
+                        "errors": [],
+                        "message": "Internal server error.",
+                        "status": 500,
+                    },
+                },
+            },
+        },
+    },
+)
 api_router.include_router(users.router, tags=["Users"])
 api_router.include_router(auth.router, tags=["Auth"])
