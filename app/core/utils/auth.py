20bf9b69b9e2a59b43223d087a12a291f7ec2611
kramber <kramber268@gmail.com>
2024-06-13 18:19:21 +0300

    feat: Add functions to manage jwt token generation

--- app/core/utils/auth.py
+++ app/core/utils/auth.py
@@ -1,12 +1,68 @@
+import datetime
+from typing import Literal
+
 import jwt
 
 from app.core.config import settings
 
 
-def encode_jwt(
-    data: dict,
-    secret: str,
-    algorithm: str = settings.
+def _encode_jwt(
+    jwt_type: Literal["access", "refresh"],
+    payload: dict[str, str | int],
+    key: str = settings.jwt.SECRET,
+    algorithm: str = settings.jwt.ALGORITHM,
 ) -> str:
 
-    return jwt.encode(data, secret, algorithm=algorithm)
+    now: int = int(datetime.datetime.now(datetime.UTC).timestamp())
+
+    if jwt_type == "access":
+        expire: int = now + settings.jwt.ACCESS_TOKEN_EXPIRE_MINUTES * 60
+    else:
+        expire = now + settings.jwt.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 60 * 60
+
+    payload.update(
+        {
+            "exp": expire,
+            "iat": now,
+        },
+    )
+    return jwt.encode(
+        payload,
+        key,
+        algorithm,
+        headers={
+            "typ": jwt_type,
+        },
+    )
+
+
+def generate_access_token(
+    user_id: int,
+    name: str,
+    email: str,
+) -> str:
+
+    return _encode_jwt(
+        jwt_type="access",
+        payload={
+            "sub": str(user_id),
+            "name": name,
+            "email": email,
+        },
+    )
+
+
+def generate_refresh_token(
+    user_id: int,
+    name: str,
+    email: str,
+) -> str:
+
+    return _encode_jwt(
+        jwt_type="refresh",
+        payload={
+            "sub": str(user_id),
+            "name": name,
+            "email": email,
+        },
+    )
