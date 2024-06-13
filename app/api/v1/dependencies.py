abfe84b7adce0a223000c52583c3536c39f8be60
kramber <kramber268@gmail.com>
2024-06-13 20:46:11 +0300

    feat: Remove dependencies file to make code more readable

--- app/api/v1/dependencies.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.core.database import db
-
-SessionDependency = Annotated[AsyncSession, Depends(db.scoped_session_dependency)]
