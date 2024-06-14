79193ad56b4778dcfbebb7c23868f6a7be3848c6
kramber1024 <kramber268@gmail.com>
2024-06-14 14:26:33 +0300

    feat: Add active field

--- app/core/database/models/user.py
+++ app/core/database/models/user.py
@@ -1,5 +1,5 @@
 from bcrypt import checkpw, gensalt, hashpw
-from sqlalchemy import String
+from sqlalchemy import Boolean, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -11,12 +11,14 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(60), nullable=False)
+    active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def __init__(self, *, name: str, email: str, password: str) -> None:
         super().__init__()
         self.name = name
         self.email = email
         self.password = hashpw(password.encode("utf-8"), gensalt(rounds=15)).decode()
+        self.active = True
 
     def __repr__(self) -> str:
         return f"<User {self.id} {self.name}>"
