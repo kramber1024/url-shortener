681847dd1af11f03ba39cf18268d08abb2f6359d
kramber1024 <kramber268@gmail.com>
2024-06-14 14:19:46 +0300

    feat: Update function because User class automatically hashes password and password needs to be in plain text for testing

--- app/tests/utils/user.py
+++ app/tests/utils/user.py
@@ -2,9 +2,9 @@ from app.core.database.models import User
 from app.tests.utils.utils import random_email, random_string_of_random_length
 
 
-def random_user() -> User:
-    return User(
-        name=random_string_of_random_length(1, User.name.type.length),
-        email=random_email(max_length=User.email.type.length),
-        password=random_string_of_random_length(8, User.password.type.length),
+def random_user_credentials() -> tuple[str, str, str]:
+    return (
+        random_string_of_random_length(1, User.name.type.length),
+        random_email(max_length=User.email.type.length),
+        random_string_of_random_length(8, User.password.type.length),
     )
