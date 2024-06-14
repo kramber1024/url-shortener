d7bf99183d5446a5e9652ac8da96ee69b8af454d
kramber1024 <kramber268@gmail.com>
2024-06-14 14:14:53 +0300

    feat: Update init file

--- app/tests/utils/__init__.py
+++ app/tests/utils/__init__.py
@@ -1,7 +1,9 @@
+from .user import random_user
 from .utils import random_email, random_string, random_string_of_random_length
 
 __all__ = (
     "random_string",
     "random_string_of_random_length",
     "random_email",
+    "random_user",
 )
