3bd5543e7e66900a2da057ad39bd5efd2ec597c0
kramber1024 <kramber268@gmail.com>
2024-06-15 12:54:01 +0300

    feat: Update tests

--- tests/api/v1/routes/test_auth.py
+++ tests/api/v1/routes/test_auth.py
@@ -48,6 +48,7 @@ if TYPE_CHECKING:
     ],
 )
 async def test_register_user_validation_error(
+    session: AsyncSession,
     client: AsyncClient,
     name: str | None,
     email: str | None,
@@ -71,6 +72,15 @@ async def test_register_user_validation_error(
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
+    email = "" if email is None else email
+
+    user: User | None = await crud.get_user_by_email(
+        session=session,
+        email=email,
+    )
+
+    assert user is None
+
 
 @pytest.mark.asyncio()
 async def test_register_user_email_conflict(
@@ -78,6 +88,7 @@ async def test_register_user_email_conflict(
     client: AsyncClient,
 ) -> None:
     name, email, password = utils.random_user_credentials()
+    formatted_email: str = utils.format_email(email)
 
     user: User = await crud.create_user(
         session=session,
@@ -89,10 +100,22 @@ async def test_register_user_email_conflict(
     response: Response = await client.post(
         "/api/v1/auth/register",
         json={
-            "name": name,
-            "email": user.email,
-            "password": user.password,
+            "name": utils.random_string_of_random_length(4, 16),
+            "email": email,
+            "password": utils.random_string_of_random_length(32, 64),
         },
     )
 
     assert response.status_code == status.HTTP_409_CONFLICT
+
+    found_user: User | None = await crud.get_user_by_email(
+        session=session,
+        email=formatted_email,
+    )
+
+    assert found_user is not None
+    assert found_user.id == user.id
+    assert found_user.name == user.name
+    assert found_user.email == user.email
+    assert found_user.password == user.password
+    assert found_user.active == user.active
