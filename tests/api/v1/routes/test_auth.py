51facf96bd2454f12a47c6052439b007f6ba94bb
kramber1024 <kramber268@gmail.com>
2024-06-15 13:46:36 +0300

    feat: Add tests for /auth/ ednpoints

--- tests/api/v1/routes/test_auth.py
+++ tests/api/v1/routes/test_auth.py
@@ -119,3 +119,197 @@ async def test_register_user_email_conflict(
     assert found_user.email == user.email
     assert found_user.password == user.password
     assert found_user.active == user.active
+
+
+@pytest.mark.asyncio()
+async def test_register_user_success(
+    session: AsyncSession,
+    client: AsyncClient,
+) -> None:
+    name, email, password = utils.random_user_credentials()
+    formatted_email: str = utils.format_email(email)
+
+    response: Response = await client.post(
+        "/api/v1/auth/register",
+        json={
+            "name": name,
+            "email": email,
+            "password": password,
+        },
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    found_user: User | None = await crud.get_user_by_email(
+        session=session,
+        email=formatted_email,
+    )
+
+    assert found_user is not None
+    assert found_user.name == name
+    assert found_user.email == formatted_email
+    assert found_user.password != password
+    assert found_user.active
+
+
+@pytest.mark.asyncio()
+@pytest.mark.parametrize(
+    ("email", "password"), [
+        (
+            f"{utils.random_string(5)}@{utils.random_string(5)}", # Invalid email
+            utils.random_string(16),
+        ),
+        (
+            "", # Invalid email
+            f"{utils.random_string(5)}@{utils.random_string(15)}",
+        ),
+        (
+            utils.random_string(5), # Invalid email
+            utils.random_string(1024), # Invalid password
+        ),
+        (
+            utils.random_string_of_random_length(4, 16), # Invalid email
+            utils.random_string(5), # Invalid password
+        ),
+        (
+            None, None, # No email, and password
+        ),
+        (
+            "", # Invalid email
+            "", # Invalid password
+        ),
+    ],
+)
+async def test_authenticate_user_validation_error(
+    session: AsyncSession,
+    client: AsyncClient,
+    email: str | None,
+    password: str | None,
+) -> None:
+
+    json: dict[str, str] = {}
+
+    if email is not None:
+        json["email"] = email
+
+    if password is not None:
+        json["password"] = password
+
+    response: Response = await client.post(
+        "/api/v1/auth/login",
+        json=json,
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    email = "" if email is None else email
+
+    user: User | None = await crud.get_user_by_email(
+        session=session,
+        email=email,
+    )
+
+    assert user is None
+
+
+@pytest.mark.asyncio()
+async def test_authenticate_user_incorrect_credentials(
+    session: AsyncSession,
+    client: AsyncClient,
+) -> None:
+
+    name, email, password = utils.random_user_credentials()
+    formatted_email: str = utils.format_email(email)
+
+    user: User = await crud.create_user(
+        session=session,
+        name=name,
+        email=email,
+        password=password,
+    )
+
+    response_1: Response = await client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": email,
+            "password": password[:-5] + utils.random_string(5),
+        },
+    )
+
+    assert response_1.status_code == status.HTTP_401_UNAUTHORIZED
+
+    response_2: Response = await client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": utils.random_email(),
+            "password": password,
+        },
+    )
+
+    assert response_2.status_code == status.HTTP_401_UNAUTHORIZED
+
+    response_3: Response = await client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": email,
+            "password": user.password,
+        },
+    )
+
+    assert response_3.status_code == status.HTTP_401_UNAUTHORIZED
+
+    found_user: User | None = await crud.get_user_by_email(
+        session=session,
+        email=formatted_email,
+    )
+
+    assert found_user is not None
+    assert found_user.name == name
+    assert found_user.email == formatted_email
+    assert found_user.password != password
+    assert found_user.active
+
+
+@pytest.mark.asyncio()
+async def test_authenticate_user_success(
+    session: AsyncSession,
+    client: AsyncClient,
+) -> None:
+
+    name, email, password = utils.random_user_credentials()
+    formatted_email: str = utils.format_email(email)
+
+    user: User = await crud.create_user(
+        session=session,
+        name=name,
+        email=email,
+        password=password,
+    )
+
+    response: Response = await client.post(
+        "/api/v1/auth/login",
+        json={
+            "email": email,
+            "password": password,
+        },
+    )
+
+    response_body: dict[str, str] = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response_body["access_token"]
+    assert response_body["refresh_token"]
+    assert isinstance(response_body["access_token"], str)
+    assert isinstance(response_body["refresh_token"], str)
+
+    found_user: User | None = await crud.get_user_by_email(
+        session=session,
+        email=formatted_email,
+    )
+
+    assert found_user is not None
+    assert found_user.id == user.id
+    assert found_user.name == name
+    assert found_user.email == formatted_email
+    assert found_user.password != password
+    assert found_user.active
