673a36e0ed0c0590f6b74e88b008f38ea7e71dd7
kramber1024 <kramber268@gmail.com>
2024-06-16 22:43:21 +0300

    feat: Add argument to control salt rounds

--- app/core/database/models/user.py
+++ app/core/database/models/user.py
@@ -1,4 +1,4 @@
-from bcrypt import checkpw, gensalt, hashpw
+import bcrypt
 from sqlalchemy import Boolean, String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -13,15 +13,27 @@ class User(Base):
     password: Mapped[str] = mapped_column(String(60), nullable=False)
     active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-    def __init__(self, *, name: str, email: str, password: str) -> None:
+    def __init__(
+        self,
+        *,
+        name: str,
+        email: str,
+        password: str,
+        salt_rounds: int = 15,
+    ) -> None:
+
         super().__init__()
         self.name = name
-        self.email = email.split("@")[0] + "@" + email.split("@")[1].lower()
-        self.password = hashpw(password.encode("utf-8"), gensalt(rounds=15)).decode()
+        self.email = email.split("@")[0] \
+            + f"@{email.split("@")[1].lower()}" if "@" in email else ""
+        self.password = bcrypt.hashpw(
+            password.encode("utf-8"),
+            bcrypt.gensalt(rounds=salt_rounds),
+        ).decode()
         self.active = True
 
     def __repr__(self) -> str:
         return f"<User {self.id} {self.name}>"
 
     def is_password_valid(self, password: str) -> bool:
-        return checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
+        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
