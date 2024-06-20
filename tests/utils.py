9f9bbc8e7669b300e070d54b97a51496ce04bb00
kramber1024 <kramber268@gmail.com>
2024-06-20 14:23:08 +0300

    feat: Add function to test if error type exists

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,11 @@
+from typing import Any
+
+
+def error_type_exists(json: dict[str, Any], error_type: str) -> bool:
+    errors: list[dict[str, Any]] = json.get("errors", [])
+    return any(error.get("type") == error_type for error in errors)
+
+
 def format_email(email: str) -> str:
     return email.split("@")[0] \
         + f"@{email.split("@")[1].lower()}" if "@" in email else ""
