28a10c83eddfc99c41d851cb5e9c6a9b99dccb72
kramber1024 <kramber268@gmail.com>
2024-07-01 01:23:18 +0300

    refactor: Move some repeating fields to separate file feat: Update name fields on all users schemes

--- backend/api/v1/schemes.py
+++ backend/api/v1/schemes.py
@@ -1,8 +1,10 @@
 from typing import Annotated, Literal
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 
 from backend.core.database.models import User as UserModel
+
+from .fields import Email, FirstName, HTTPStatus, Id, LastName, Password
 
 
 class Error(BaseModel):
@@ -42,15 +44,7 @@ class ErrorResponse(BaseModel):
             examples=["Validation error."],
         ),
     ]
-    status: Annotated[
-        int,
-        Field(
-            description="HTTP status code.",
-            examples=[422],
-            ge=100,
-            le=599,
-        ),
-    ]
+    status: HTTPStatus
 
 
 class SuccessResponse(BaseModel):
@@ -61,45 +55,14 @@ class SuccessResponse(BaseModel):
             examples=["Operation completed successfully."],
         ),
     ]
-    status: Annotated[
-        int,
-        Field(
-            description="HTTP status code.",
-            examples=[200],
-            ge=100,
-            le=599,
-        ),
-    ]
+    status: HTTPStatus
 
 
 class UserRegistration(BaseModel):
-    name: Annotated[
-        str,
-        Field(
-            min_length=3,
-            max_length=32,
-            description="Displayed in User's profile.",
-            examples=["kramber"],
-        ),
-    ]
-    email: Annotated[
-        EmailStr,
-        Field(
-            min_length=len("*@*.*"),
-            max_length=64,
-            description="Used for authentication and notifications.",
-            examples=["email@domain.tld"],
-        ),
-    ]
-    password: Annotated[
-        str,
-        Field(
-            min_length=8,
-            max_length=256,
-            description="Used for authentication.",
-            examples=["My$uper$ecretPa$$word"],
-        ),
-    ]
+    first_name: FirstName
+    last_name: LastName | None
+    email: Email
+    password: Password
     terms: Annotated[
         Literal["on"],
         Field(
@@ -110,59 +73,21 @@ class UserRegistration(BaseModel):
 
 
 class UserLogin(BaseModel):
-    email: Annotated[
-        EmailStr,
-        Field(
-            min_length=len("*@*.*"),
-            max_length=64,
-            description="Email used for authentication.",
-            examples=["email@domain.tld"],
-        ),
-    ]
-    password: Annotated[
-        str,
-        Field(
-            min_length=8,
-            max_length=256,
-            description="Password used for authentication.",
-            examples=["My$uper$ecretPa$$word"],
-        ),
-    ]
+    email: Email
+    password: Password
 
 
 class User(BaseModel):
-    id: Annotated[
-        str,
-        Field(
-            description="Unique identifier for the user.",
-            examples=["7205649978688008192"],
-            max_length=len("7205649978688008192"),
-            min_length=len("7205649978688008192"),
-        ),
-    ]
-    name: Annotated[
-        str,
-        Field(
-            min_length=3,
-            max_length=32,
-            description="Displayed in profile.",
-            examples=["kramber"],
-        ),
-    ]
-    email: Annotated[
-        EmailStr,
-        Field(
-            min_length=len("*@*.*"),
-            max_length=64,
-            description="Used for authentication.",
-            examples=["email@domain.tld"],
-        ),
-    ]
+    id: Id
+    first_name: FirstName
+    last_name: LastName | None
+    email: Email
 
     @classmethod
     def from_model(cls: type["User"], user: UserModel) -> "User":
         return cls(
             id=str(user.id),
-            name=user.name,
+            first_name=user.first_name,
+            last_name=user.last_name,
             email=user.email,
         )
