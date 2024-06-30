451230a6c762fd6ee04d5c8fac473a9b6c901b5e
kramber1024 <kramber268@gmail.com>
2024-07-01 00:17:27 +0300

    feat: Add SALT_ROUNDS configuration option

--- backend/core/config.py
+++ backend/core/config.py
@@ -14,6 +14,7 @@ class SnowflakeSettings(BaseSettings):
 class DatabaseSettings(BaseSettings):
     gen: SnowflakeSettings = SnowflakeSettings()
     URL: str = f"sqlite+aiosqlite:///{BASE_DIRERCTORY/"database"/"database.sqlite3"}"
+    SALT_ROUNDS: int = 16
 
 
 class JWTSettings(BaseSettings):
