c7865c1451adc53cfc79d0a13ca890f47bd5f65d
kramber1024 <kramber268@gmail.com>
2024-06-30 19:01:38 +0300

    feat: Add banned field refactor: Add function for formatting emails

--- backend/core/database/models/user.py
+++ backend/core/database/models/user.py
@@ -11,7 +11,8 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(60), nullable=False)
-    active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    banned: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    activated: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     def __init__(
         self,
@@ -24,16 +25,25 @@ class User(Base):
 
         super().__init__()
         self.name = name
-        self.email = email.split("@")[0] \
-            + f"@{email.split("@")[1].lower()}" if "@" in email else ""
+        self.email = self._format_email(email)
         self.password = bcrypt.hashpw(
             password.encode("utf-8"),
             bcrypt.gensalt(rounds=salt_rounds),
         ).decode()
-        self.active = True
+        self.banned = False
+        self.activated = False
+
+    def is_password_valid(self, password: str) -> bool:
+        return bcrypt.checkpw(
+            password.encode("utf-8"),
+            self.password.encode("utf-8"),
+        )
+
+    def _format_email(self, email: str) -> str:
+        if "@" not in email:
+            return email
+
+        return f"{email.split("@")[0]}@{email.split('@')[1].lower()}"
 
     def __repr__(self) -> str:
         return f"<User {self.id} {self.name}>"
-
-    def is_password_valid(self, password: str) -> bool:
-        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
