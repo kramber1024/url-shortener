3781ba7301232ac7cd345837caac5de67d6db0e2
kramber1024 <kramber268@gmail.com>
2024-07-02 22:47:46 +0300

    feat: Add few consts for tests feat: Add format_email function

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,14 @@
 from typing import Any
 
+import bcrypt
+
+SNOWFLAKE_RANGE: range = range(10**18, 10**19)
+DB_USER_PASSWORD: str = "yK0jcTEZKd57NNf092a:1eb5:9148:44d6:077d:f71b:4936:0bb4"
+DB_USER_PASSWORD_HASH: str = bcrypt.hashpw(
+    DB_USER_PASSWORD.encode("utf-8"),
+    bcrypt.gensalt(4),
+).decode("utf-8")
+
 
 def error_type_exists(json: dict[str, Any], error_type: str) -> bool:
     errors: list[dict[str, Any]] = json.get("errors", [])
@@ -7,5 +16,8 @@ def error_type_exists(json: dict[str, Any], error_type: str) -> bool:
 
 
 def format_email(email: str) -> str:
-    return email.split("@")[0] \
-        + f"@{email.split("@")[1].lower()}" if "@" in email else ""
+    if "@" not in email:
+        return email
+    email_splitted: list[str] = email.split("@")
+
+    return f"{email_splitted[0]}@{email_splitted[1].lower()}"
