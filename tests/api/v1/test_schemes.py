6cc392045d2fed17604ce14aa2285769b8ac7e9e
kramber1024 <kramber268@gmail.com>
2024-07-03 15:42:28 +0300

    fix: Replace name argument with first_name and last_name arguments

--- tests/api/v1/test_schemes.py
+++ tests/api/v1/test_schemes.py
@@ -3,20 +3,22 @@ from backend.core.database.models import User as UserModel
 
 
 def test_user_scheme() -> None:
-    name: str = "Dillon.Howe"
+    first_name: str = "Dillon"
+    last_name: str = "Carroll"
     email: str = "Dovie87@gmail.com"
     password: str = "YzbXhViCSId_pJK"
 
     user_model: UserModel = UserModel(
-        name=name,
+        first_name=first_name,
+        last_name=last_name,
         email=email,
         password=password,
-        salt_rounds=4,
     )
     user_scheme: UserScheme = UserScheme.from_model(user_model)
 
     assert isinstance(user_scheme, UserScheme)
     assert user_scheme.id == str(user_model.id)
-    assert user_scheme.name == user_model.name
+    assert user_scheme.first_name == user_model.first_name
+    assert user_scheme.last_name == user_model.last_name
     assert user_scheme.email == user_model.email
 
